11c167768ce50a542ac889368540b6705879fa07
Renier Coetsee <Renier Coetsee@DESKTOP-O5J3R5F>
2017-08-07 02:43:46 +0200

    123

---
+++ src/javafxapplication3/JavaFXApplication3.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication3;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Renier Coetsee
+ */
+public class JavaFXApplication3 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Stage theStage = stage;
+        
+        AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+       
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
